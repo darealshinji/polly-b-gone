3085fcdd753f80138cc3a8cc3d8bf8f5d0c7dc36
darealshinji <djcj@gmx.de>
2015-12-23 18:26:17 +0100

    Enable loading of resources from a hardcoded location

--- src/resource.cpp
+++ src/resource.cpp
@@ -4,12 +4,33 @@
 #include <ios>
 #include <iostream>
 
+#ifdef STORAGE_PATH
+  #include <sys/types.h>
+  #include <sys/stat.h>
+#endif
+
 #include "resource.h"
+
+struct stat info;
 
 using namespace mbostock;
 
 const char* Resources::path() {
+#ifdef __APPLE__
   return "Contents/Resources/";
+#else
+  #ifdef STORAGE_PATH
+    // check if STORAGE_PATH exists
+    // (i.e. "/usr/local/share/polly-b-gone/"),
+    // otherwise fall back to "resources/
+    if( stat( STORAGE_PATH, &info ) != 0 )
+      return "resources/";
+    else
+      return STORAGE_PATH;
+  #else
+    return "resources/";
+  #endif
+#endif
 }
 
 const char* Resources::readFile(const char* p) {
